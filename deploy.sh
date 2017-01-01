ef7d11eda64f7cfd6057e12836e7a692c8a91cb1
Anatolii Sakhnik <sakhnik@gmail.com>
2017-01-01 16:25:12 +0200

    Improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+set -e
+
 export JEKYLL_ENV=production
 
-jekyll build --incremental
+this_dir=`dirname ${BASH_SOURCE[0]}`
+
+mkdir -p _site
+mkdir -p /tmp/sakhnik.com-production
+
+bindfs -n /tmp/sakhnik.com-production $this_dir/_site
+
+cleanup()
+{
+	fusermount -u _site
+}
+
+trap cleanup EXIT
+
+jekyll build
+
 rsync -raP --chown 33:33 _site/* iryska.do:/var/www/sakhnik.com
