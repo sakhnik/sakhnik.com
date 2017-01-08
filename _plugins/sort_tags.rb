656f234f82490fd09922f6cf1bd1438e0ef9053e
Anatolii Sakhnik <sakhnik@gmail.com>
2017-01-08 09:03:57 +0200

    Produce list of tags sorted by count of posts

---
+++ _plugins/sort_tags.rb
@@ -0,0 +1,14 @@
+module Jekyll
+  module TagHelpersFilter
+    def sort_tags_by_posts_count(tags, language)
+      puts tags
+                                                            # First filter out posts by language
+      return tags.map     { |k,v| [k, v.select {|a| a.data['lang'] == language}] }
+                 .reject  { |k,v| v.empty? }                # Filter out empty tags
+                 .map     { |k,v| [ k, v.size ] }           # Count of posts for a tag
+                 .sort_by { |x| [ -x[1], x[0].downcase ] }  # Sort finally descending
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::TagHelpersFilter)
