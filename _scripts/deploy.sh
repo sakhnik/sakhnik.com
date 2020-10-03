42024631bdd6d4f69c722da51548cac2dae8013b
Anatolii Sakhnik <sakhnik@gmail.com>
2020-10-03 22:39:51 +0300

    Remove directory bind, no need for it anymore

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,20 +2,6 @@
 
 export JEKYLL_ENV=production
 
-root_dir=`dirname ${BASH_SOURCE[0]}`/..
-
-mkdir -p _site
-mkdir -p /tmp/sakhnik.com-production
-
-bindfs -n /tmp/sakhnik.com-production $root_dir/_site
-
-cleanup()
-{
-	fusermount -u _site
-}
-
-trap cleanup EXIT
-
 bundle exec jekyll build
 
 ssh iryska.do 'rm -rf /var/www/sakhnik.com/*'
