2004ff411f80b86ccdf33294ca7d44e924bb3b20
Anatolii Sakhnik <sakhnik@gmail.com>
2020-11-22 21:31:11 +0200

    Update the host alias

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,5 +4,7 @@ export JEKYLL_ENV=production
 
 bundle exec jekyll build
 
-ssh iryska.do 'rm -rf /var/www/sakhnik.com/*'
-rsync -raP --chown 33:33 _site/* iryska.do:/var/www/sakhnik.com
+host=iryska
+
+ssh $host 'rm -rf /var/www/sakhnik.com/*'
+rsync -raP --chown 33:33 _site/* $host:/var/www/sakhnik.com
